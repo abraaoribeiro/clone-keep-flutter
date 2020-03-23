1576a25805bd59e4977f57383c08b60005d11ad4
Abraao Ribeiro <abraaohdg@gmail.com>
2020-03-23 14:11:29 -0300

    estrutura de pasta do projeto

--- lib/app/app_module.dart
+++ lib/app/app_module.dart
@@ -1,8 +1,8 @@
 import 'package:clone_keep_flutter/app/app_controller.dart';
+import 'package:clone_keep_flutter/app/pages/home/home_module.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:flutter/material.dart';
 import 'package:clone_keep_flutter/app/app_widget.dart';
-import 'package:clone_keep_flutter/app/modules/home/home_module.dart';
 
 class AppModule extends MainModule {
   @override
--- test/app/pages/home/home_controller_test.dart
+++ test/app/pages/home/home_controller_test.dart
@@ -1,7 +1,7 @@
+import 'package:clone_keep_flutter/app/pages/home/home_module.dart';
 import 'package:flutter_modular/flutter_modular_test.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import '../../../../lib/app/modules/home/home_module.dart';
 
 
 
